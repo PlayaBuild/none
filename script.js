d29aab90e675ab380ddd488a438c6d08d4eb4934
PlayaBuild <playabuildllc@gmail.com>
2025-08-03 11:35:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
+fetch("/")
+  .then((response) => response.text())
+  .then((data) => {
+    console.log("✅ Backend says:", data);
+    document.body.innerHTML = `<h2>${data}</h2>`;
+  })
   .catch((error) => {
     console.error("❌ Error connecting to backend:", error);
     document.body.innerHTML = `<h2 style="color:red;">Backend error</h2>`;
   });
-<script>
-  document.getElementById('agreeCheckbox').addEventListener('change', function () {
-    const button = document.getElementById('recruitBtn');
-    button.disabled = !this.checked;
-  });
-</script>
